d0b335816ea0ffdc0976a8c595f116c8db4f89a6
ganeshdipdumbare <ubuntu@ip-172-31-17-100.us-east-2.compute.internal>
2018-07-09 04:35:49 +0000

    Changes migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
+var TaskeeLibrary = artifacts.require("./TaskeeLibrary.sol");
 var TaskoOrganisation = artifacts.require("./TaskoOrganisation.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(TaskoOrganisation);
+  deployer.deploy(TaskeeLibrary).then(function(){
+    deployer.link(TaskeeLibrary,TaskoOrganisation);
+    deployer.deploy(TaskoOrganisation);
+  });
 };
